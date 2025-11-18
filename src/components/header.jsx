2616faa928774ef6092b3927fe933aa8c12be4ac
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-11-18 08:27:18 -0300

    feat: add Header component with logo and navigation

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header className={``}>
+            <div className={``}>
+                <img src="../assets/logo.svg" alt="Rede de Alívio logo" className={``} />
+                <h1 className={``}>Rede de Alívio</h1>
+            </div>
+            <div className={``}>
+                <a href="/">Meus Relatos</a>
+            </div>
+        </header>
+    );
+}
