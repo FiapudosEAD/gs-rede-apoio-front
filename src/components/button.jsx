1e76d85b232fefb27ba0a7891cdb9efe77675468
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-11-18 13:59:44 -0300

    add: styling button and header components

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,8 @@
 export default function Button({ label, redirectUrl }) {
   return (
     <button 
-      className=""
+      className="bg-light-green text-dark-green font-bold text-sm h-7 p-1 md:p-2 md:text-xl md:h-10 items-center rounded-md 
+      hover:ease-in-out hover:scale-105 transition-transform duration-200"
       onClick={() => window.location.href = redirectUrl}
     >
       {label}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,12 @@ import Button from "./button.jsx";
 
 export default function Header() {
     return (
-        <div className="">
-            <div className="">
-                <img src="./src/assets/logo.svg" alt="Rede de Alívio logo" className="" />
-                <h1 className="">Rede de Alívio</h1>
+        <div className="bg-dark-green flex items-center justify-between p-5 md:text-xl text-sm text-white">
+            <div className="flex items-center">
+                <img src="./src/assets/logo.svg" alt="Rede de Alívio logo" className="w-10 md:w-15 xl:w-20" />
+                <h1 className="font-bold md:text-xl xl:text-3xl">Rede de Alívio</h1>
             </div>
-            <div className="">
+            <div className="flex items-center gap-2 md:gap-6 xl:gap-10">
                 <a href="/" className="">Meus Relatos</a>
                 <Button label={"Escrever Relato"} redirectUrl={"/"}></Button>
             </div>
