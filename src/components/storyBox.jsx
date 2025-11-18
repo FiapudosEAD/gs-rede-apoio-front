1978e4c78f35ef8de1fb8b9ea2079e093ebec67e
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-11-18 09:24:07 -0300

    feat: add StoryBox component to display title, content, and like count

---
+++ src/components/storyBox.jsx
@@ -0,0 +1,20 @@
+import { HeartIcon } from '@heroicons/react/24/outline';
+
+export default function StoryBox({ title, content, likeCount }) {
+  return (
+    <div>
+        <h1>
+            {title}
+        </h1>
+        <div></div>
+        <div>
+            {content}
+        </div>
+        <div>
+            <HeartIcon className="h-5 w-5 inline-block mr-2 text-red-500" aria-hidden="true" />
+            {likeCount}
+        </div>
+    </div>
+
+  );
+}
