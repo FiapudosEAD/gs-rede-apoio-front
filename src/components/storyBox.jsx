866acae7044ebe4eb2c6dded38fae8cddf464a44
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-11-18 09:14:45 -0300

    feat: add @heroicons/react dependency and update StoryBox component to use PencilSquareIcon

--- src/components/storyBox.jsx
+++ src/components/storyBox.jsx
@@ -1,4 +1,6 @@
 
+import { PencilSquareIcon } from '@heroicons/react/24/outline';
+
 export default function StoryBox({ title, likeCount, commentCount }) {
   return (
     <div>
@@ -9,7 +11,7 @@ export default function StoryBox({ title, likeCount, commentCount }) {
         </div>
         <div className="">
             <span className="">
-                <pencil-square className="" />
+                <PencilSquareIcon className="h-5 w-5 inline-block mr-2 text-gray-500" aria-hidden="true" />
                 <a href="/">Editar</a>
             </span>
         </div>
