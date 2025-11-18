5d8c0e7972f1d70a19317833cf7450aa1e582366
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-11-18 15:03:12 -0300

    add: styles for miniboxstory component and edit home page

--- src/components/miniStoryBox.jsx
+++ src/components/miniStoryBox.jsx
@@ -1,20 +1,25 @@
-
-import { PencilSquareIcon } from '@heroicons/react/24/outline';
+import { PencilSquareIcon, HeartIcon, ChatBubbleLeftIcon } from '@heroicons/react/24/outline';
 
 export default function MiniStoryBox({ title, likeCount, commentCount }) {
   return (
-    <div>
-        <h2 className="">{title}</h2>
-        <div className="">
-            <span className=""> {likeCount}</span>
-            <span className=""> {commentCount}</span>
-        </div>
-        <div className="">
-            <span className="">
-                <PencilSquareIcon className="h-5 w-5 inline-block mr-2 text-gray-500" aria-hidden="true" />
-                <a href="/">Editar</a>
+    <div className="bg-light-blue p-10 rounded-md gap-6 flex flex-col shadow-md md:p-15 font-bold hover:ease-in-out hover:scale-105 transition-transform duration-200">
+        <h2 className="text-2xl self-start sm:self-auto">{title}</h2>
+        <div className="text-blue-accent flex gap-6 items-center self-end sm:self-auto md:justify-end">
+            <span className="flex gap-2 items-center">
+              <HeartIcon className="h-5 w-5" aria-hidden="true" />
+              {likeCount}
+            </span>
+            <span className="flex gap-2 items-center">
+              <ChatBubbleLeftIcon className="h-5 w-5" aria-hidden="true" />
+              {commentCount}
             </span>
         </div>
+        {/* <div className="">
+            <span className="">
+                <PencilSquareIcon className="h-5 w-5 inline-block mr-2" aria-hidden="true" />
+                <a href="/">Editar</a>
+            </span>
+        </div> */}
     </div>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,19 @@
 import Header from "../components/header.jsx";
+import MiniStoryBox from "../components/miniStoryBox.jsx";
 
 export default function Home() {
-	return (
+    return (
         <>
             <Header />
+            <h1 className="p-5 font-bold text-3xl">Relatos de Outras Pessoas</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 p-5 gap-5 place-items-center">
+                <MiniStoryBox title="Título do Relato" likeCount="100" commentCount="100"/>
+                <MiniStoryBox title="Título do Relato" likeCount="100" commentCount="100"/>
+                <MiniStoryBox title="Título do Relato" likeCount="100" commentCount="100"/>
+                <MiniStoryBox title="Título do Relato" likeCount="100" commentCount="100"/>
+                <MiniStoryBox title="Título do Relato" likeCount="100" commentCount="100"/>
+                <MiniStoryBox title="Título do Relato" likeCount="100" commentCount="100"/>
+            </div>
         </>
     )
 }
