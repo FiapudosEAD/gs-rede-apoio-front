9e4ffd6c05a005ed7ac9868727864fc7d7808604
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-11-20 08:21:35 -0300

    fix: correct props value add onChange

--- src/components/textAreaLabel.jsx
+++ src/components/textAreaLabel.jsx
@@ -1,9 +1,11 @@
-export default function TextAreaLabel({ placeholder }) {
+export default function TextAreaLabel({ placeholder, value, onChange }) {
     return (
         <textarea
             className="bg-white font-bold text-dark-green text-md p-2 rounded-md 
-            shadow-md border-2 border-dark-green resize-none h-40 md:h-60"
+            shadow-md border-2 border-dark-green resize-none h-40 md:h-60 focus:outline-none focus:border-light-green"
             placeholder={placeholder}
+            value={value}
+            onChange={onChange}
         />    
     );
 }
