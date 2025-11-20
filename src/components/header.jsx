3921dc023f72138ff3f7a183e41a6d7bf04d0a96
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-11-20 09:43:34 -0300

    fix: redirect url to home

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <div className="bg-dark-green flex items-center justify-between p-5 md:text-xl text-sm text-white">
             <div className="flex items-center">
                 <img src="../assets/logo.svg" alt="Rede de Alívio logo" className="w-10 md:w-15 xl:w-20" />
-                <h1 className="font-bold md:text-xl xl:text-3xl">Rede de Alívio</h1>
+                <h1 className="font-bold md:text-xl xl:text-3xl"><a href="/">Rede de Alívio</a></h1>
             </div>
             <div className="flex items-center gap-2 md:gap-6 xl:gap-10">
                 <a href="/myStories" className="">Meus Relatos</a>
