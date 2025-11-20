4d4ea08d29f22eb6138eef3f991aa96363617a7e
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-11-20 08:16:58 -0300

    add: add UserContext.js to verify the user logged

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import { UserProvider } from "./contexts/UserContext.jsx";
 
 // Importação das Páginas
 import Home from "./pages/home.jsx";
@@ -24,6 +25,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );
